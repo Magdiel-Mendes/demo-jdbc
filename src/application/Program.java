6942565325f4ef11a96982195d8d691c550645f7
Magdiel <santosmagdiel931@outlook.pt>
2023-07-02 16:30:29 -0300

    primeiro teste findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,16 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
 
-import db.DB;
-import db.DbException;
-import db.DbIntegrityExceptioin;
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
 public class Program {
 	 
 	public static void main(String[] args) {
-		Department obj = new  Department(1, "books");
-		System.out.println(obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
+		
 	}
 }

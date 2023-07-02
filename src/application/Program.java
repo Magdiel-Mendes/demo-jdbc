d57caf3bc27b686a56d5c24da44a0cc2e0fc53d9
Magdiel <santosmagdiel931@outlook.pt>
2023-07-01 22:29:37 -0300

    estrutura inicial

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,42 +8,11 @@ import java.sql.Statement;
 import db.DB;
 import db.DbException;
 import db.DbIntegrityExceptioin;
+import model.entities.Department;
 public class Program {
 	 
 	public static void main(String[] args) {
- 
-		Connection conn = null;
-		Statement st = null;
-		try {
-			conn = DB.getConnection();
-			
-			// todas as operações ficaram pendentes esperando confirmação
-			conn.setAutoCommit(false);
-			st = conn.createStatement();
-			
-			
-			int rows  = st.executeUpdate("UPDATE seller SET BaseSalary = 20000 WHERE DepartmentId = 1");
-//			int x = 1;
-//			if(x < 2) {
-//				throw new SQLException("Fake error!");
-//			}
-			int rows2  = st.executeUpdate("UPDATE seller SET BaseSalary = 40000 WHERE DepartmentId = 2");
-			conn.commit();
-			System.out.println("rows " + rows);
-			System.out.println("rows " + rows2);
-			
-		}
-		catch (SQLException e) {
-			try {
-				conn.rollback();
-				throw new DbException("transaction rolled back! Caused: " + e.getMessage());
-			} catch (SQLException el) {
-				throw new DbException("Error trying to rollback! Caused by: " + el.getMessage());
-			}
-		} 
-		finally {
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
+		Department obj = new  Department(1, "books");
+		System.out.println(obj);
 	}
 }
